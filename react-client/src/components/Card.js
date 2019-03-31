6ad403be392254eeb3ce8823e1ca3a7ed8eca6d8
czjan23 <czjan23@gmail.com>
2019-03-31 16:47:48 -0500

    add carriers img

--- react-client/src/components/Card.js
+++ react-client/src/components/Card.js
@@ -4,23 +4,27 @@ import './Card.css';
 
 class Card extends React.Component {
   render() {
+    let code = this.props.flight.pieces[0].carrierCode;
     return (
       <div className='card-outline'>
       <div>
-        <h3>total price: ${this.props.flight.totalPrice}</h3>
+        <img alt="carrier-img" src={require('../carrierImgs/' + code + '.png')} />
         <h3>total time: {this.props.flight.totalTime}</h3>
+        <h3>total price: ${this.props.flight.totalPrice}</h3>
+        <h3>rate</h3>
         <Divider />
         {this.props.flight.pieces.map((piece, index) => {
-            return (
-                <div key={index}>
-                    <p>{piece.carrier} {piece.number}</p>
-                    <p>{piece.departureAirport} - {piece.arrivalAirport}</p>
-                    <p>{piece.departureTime} - {piece.arrivalTime}</p>
-                    <Divider />
-                    {index !== this.props.flight.pieces.length - 1 ? <h4>Wait {this.props.flight.intervals[index]}</h4> : ''}
-                    {index !== this.props.flight.pieces.length - 1 ? <Divider /> : ''}
-                </div>
-            )
+          return (
+            <div key={index}>
+                <p>{piece.carrierCode} {piece.number}</p>
+                <p>{piece.departureAirport} - {piece.arrivalAirport}</p>
+                <p>{piece.departureTime} - {piece.arrivalTime}</p>
+                <p>{piece.duration}</p>
+                <Divider />
+                {index !== this.props.flight.pieces.length - 1 ? <h4>Wait {this.props.flight.intervals[index]}</h4> : ''}
+                {index !== this.props.flight.pieces.length - 1 ? <Divider /> : ''}
+            </div>
+          )
         })}
         </div>
       </div>
