9e9ac44f22271caabb471ea3d7a867e85b2aa7ce
WeitongFeng <weitongfeng@hotmail.com>
2019-03-31 00:18:29 -0500

    Bug Fix: card position when displaying dropdown

--- react-client/src/components/CardPanel.js
+++ react-client/src/components/CardPanel.js
@@ -4,7 +4,7 @@ import Card from './Card';
 class CardPanel extends Component {
   render() {
     return (
-      <div style={{paddingTop: '80px', textAlign: 'center'}}>
+      <div style={{position: 'absolute', paddingTop: '80px', textAlign: 'center', left: '50%', marginLeft: '-150px', zIndex: '-1'}}>
         {this.props.flights.map((flight) => {
           return (
             <Card key={flight.id} flight={flight} />
